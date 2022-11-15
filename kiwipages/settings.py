ab7429104323772bd5391d030933abcb4b7db45c
arthuryoobee <arthur.lewis@yoobeesouthseas.ac.nz>
2022-11-16 12:11:49 +1300

    updated 01_packages.config

--- kiwipages/settings.py
+++ kiwipages/settings.py
@@ -23,12 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-c!3cy12$jtw@*=5at380%e4ryx_$ybyc1f=_4zuta))$_j3m%e'
 
+if 'PYTHONPATH' in os.environ:
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = False
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    #ALLOWED_HOSTS = ['django-env.eba-dtrievek.us-west-2.elasticbeanstalk.com']
+    ALLOWED_HOSTS = []
+else:    
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
 
-#ALLOWED_HOSTS = ['django-env.eba-dtrievek.us-west-2.elasticbeanstalk.com']
-ALLOWED_HOSTS = []
+    #ALLOWED_HOSTS = ['django-env.eba-dtrievek.us-west-2.elasticbeanstalk.com']
+    ALLOWED_HOSTS = ['.ap-southeast-2.elasticbeanstalk.com']
 
 # Application definition
 
